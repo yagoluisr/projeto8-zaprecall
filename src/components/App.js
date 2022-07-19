f5a158c5f4735e928a9c31132c04b6b845a02137
yagoluis <yagoluisroberto@gmail.com>
2022-07-19 11:29:16 -0300

    Tela1: Layout concluído

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div>
+            <div className="inicio">
+            <img src="Assets/Raio.png"/>
+            <span>ZapRecall</span>
+                <div className="button">
+                    <p>Iniciar Recall!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default App;
