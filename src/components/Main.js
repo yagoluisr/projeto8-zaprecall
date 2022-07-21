94067bae71b22711c80323b88af0eab25e9bb15f
yagoluis <yagoluisroberto@gmail.com>
2022-07-21 15:44:13 -0300

    Flashcard respondido: estado e icone implementado(Tela4)

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,37 +5,37 @@ function Deck(){
     return ([
         {pergunta: "O que é JSX? ", resposta:"Uma extensão de linguagem do JavaScript"},
         {pergunta: "O React é __", resposta:"Uma biblioteca JavaScript para construção de interfaces"},
-        // {pergunta: "Componentes devem iniciar com __ ", resposta:"Letra maiúscula"},
-        // {pergunta: "Podemos colocar __ dentro do JSX", resposta:"Expressões"},
-        // {pergunta: "O ReactDOM nos ajuda __ ", resposta:"Interagindo com a DOM para colocar componentes React na mesma"},
-        // {pergunta: "Usamos o npm para __ ", resposta:"Gerenciar os pacotes necessários e suas dependências"},
-        // {pergunta: "Usamos props para __ ", resposta:"Passar diferentes informações para componentes "},
-        // {pergunta: " Usamos estado (state) para __", resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"},
+        {pergunta: "Componentes devem iniciar com __ ", resposta:"Letra maiúscula"},
+        {pergunta: "Podemos colocar __ dentro do JSX", resposta:"Expressões"},
+        {pergunta: "O ReactDOM nos ajuda __ ", resposta:"Interagindo com a DOM para colocar componentes React na mesma"},
+        {pergunta: "Usamos o npm para __ ", resposta:"Gerenciar os pacotes necessários e suas dependências"},
+        {pergunta: "Usamos props para __ ", resposta:"Passar diferentes informações para componentes "},
+        {pergunta: " Usamos estado (state) para __", resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"},
     ])
 }
 
-function TemplatePergunta({setEstado}) {
+function TemplatePergunta({setEstado, index, respondido}) {
     function Pergunta(){
         setEstado("pergunta")
     }
 
     return (
-        <div className="pergunta" onClick={Pergunta}>
-            <span>Pergunta 1</span>
+        <div className={respondido} onClick={Pergunta}>
+            <span>Pergunta {index}</span>
             <img src="Assets/Vector.png" />
         </div>
     )
 }
 
 
-function TemplateFlashcard({setEstado}) {
+function TemplateFlashcard({setEstado, pergunta}) {
     function Opcoes(){
         setEstado("respostas")
     }
 
     return (
         <div className="flashcard">
-            <span>O que é JSX?</span>
+            <span>{pergunta}</span>
                 <div className="icones">
                     <img src="Assets/Girar.png" onClick={Opcoes}/>
                 </div>
@@ -43,42 +43,56 @@ function TemplateFlashcard({setEstado}) {
     )
 }
 
-function TemplateFlashcardOpcao() {
+function TemplateFlashcardOpcao({setEstado, resposta, setRespondido}) {
     return (
         <div className="flashcard">
-            <span>Uma extensão de linguagem do JavaScript</span>
+            <span>{resposta}</span>
                 <div className="opcoes">
-                    <div className="opcao1">Não lembrei</div>
-                    <div className="opcao2">Quase não lembrei</div>   
-                    <div className="opcao3">Zap!</div>
+                    <div className="opcao1" onClick={() => {setEstado("respondido"); setRespondido("errou")} }>Não lembrei</div>
+                    <div className="opcao2" onClick={() => {setEstado("respondido"); setRespondido("duvida")} }>Quase não lembrei</div>   
+                    <div className="opcao3" onClick={() => {setEstado("respondido"); setRespondido("acertou")} }>Zap!</div>
                 </div>
         </div>
     )
 }
 
+function TemplateRespondido({index, respondido}) {
 
+    return (
+        <div className={respondido}>
+            <span>Pergunta {index}</span>
+            <img src={`Assets/${respondido}.png`} />
+        </div>
+    )
+}
 
 
 export default function Main() {
     const perguntas = Deck();
     
 
-    function VerificarEstado() {
+    function VerificarEstado({index, pergunta, resposta}) {
+        const [respondido, setRespondido] = React.useState("pergunta");
         const [estado, setEstado] = React.useState("inicio");
 
         if(estado === "inicio"){
             return (
-                    <TemplatePergunta setEstado={setEstado}/>
+                <TemplatePergunta setEstado={setEstado} index={index} respondido={respondido} />
             )
         }
         if(estado === "pergunta"){
             return (
-                    <TemplateFlashcard setEstado={setEstado}/>
+                <TemplateFlashcard setEstado={setEstado} pergunta={pergunta} />
             )
         }
         if(estado === "respostas"){
             return (
-                    <TemplateFlashcardOpcao />
+                <TemplateFlashcardOpcao setEstado={setEstado} resposta={resposta} setRespondido={setRespondido}/>
+            )
+        }
+        if(estado === "respondido"){
+            return (
+                <TemplateRespondido setEstado={setEstado} index={index} respondido={respondido}/>
             )
         }
 }
@@ -86,8 +100,8 @@ export default function Main() {
     return (
         <div className="main">
             
-            {perguntas.map((value, index) => (
-                <VerificarEstado key={index} />
+            {perguntas.map((obj, index) => (
+                <VerificarEstado key={index} index={index+1} {...obj}/>
             ))}
 
         </div>
